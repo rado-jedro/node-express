2972162a751a9a2d1c52f6691b937cd46c74cd53
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-09 20:00:09 +0000

    Add middleware to serve Postman app

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
