59bce67ce52462960467306b615b10d6c84c6306
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-09 16:37:25 +0000

    Replace res.show to res.render

--- server.js
+++ server.js
@@ -17,27 +17,27 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/hello/:name', (req, res) => {
-    res.render('hello', { layout: false, name: req.params.name });
-  });
+  res.render('hello', { layout: false, name: req.params.name });
+});
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: false });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: false });
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: false });
 });
 
 app.use((req, res) => {
